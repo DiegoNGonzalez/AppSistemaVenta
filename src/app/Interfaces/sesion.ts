dedffa421d3803f1643e169bf3bb028f22061d9a
Diego Nicolas Gonzalez Valenzuela <diegonzalezv93@gmail.com>
2024-01-28 13:47:24 -0300

    creando interfaces

---
+++ src/app/Interfaces/sesion.ts
@@ -0,0 +1,7 @@
+export interface Sesion {
+    idUsuario:number,
+    nombreApellido:string,
+    email:string,
+    rolDescripcion:string
+}
+
---
+++ src/app/Interfaces/usuario.ts
@@ -0,0 +1,9 @@
+export interface Usuario {
+    idUsuario:number,
+    nombreApellido:string,
+    email:string,
+    idRol:number,
+    rolDescripcion:string,
+    clave:string,
+    esActivo:number
+}
