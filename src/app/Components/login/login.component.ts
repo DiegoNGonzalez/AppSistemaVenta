7ac29312805596ef7c81e3da9fcbb701b867e7b0
Diego Nicolas Gonzalez Valenzuela <diegonzalezv93@gmail.com>
2024-01-28 18:06:09 -0300

    logica de login

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Login } from 'src/app/Interfaces/login';
+import { UsuarioService } from 'src/app/Services/usuario.service';
+import { UtilidadService } from 'src/app/Reutilizable/utilidad.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +13,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formLogin:FormGroup;
+  ocultarPassword:boolean=true;
+  mostrarLoading:boolean=false;
+
+  constructor(
+    private fb:FormBuilder,
+    private router:Router,
+    private _usuarioService:UsuarioService,
+    private _utilidadService:UtilidadService
+  ) { 
+    this.formLogin=this.fb.group({
+      email:['',[Validators.required]],
+      clave:['',[Validators.required]]
+    });
+  }
+    
+
 
   ngOnInit(): void {
   }
 
+  iniciarSesion(){
+    this.mostrarLoading=true;
+
+    const request:Login={
+      email:this.formLogin.value.email,
+      clave:this.formLogin.value.clave
+    }
+    this._usuarioService.iniciarSesion(request).subscribe({
+      next:(data)=>{
+        if(data.status){
+          this._utilidadService.guardarSesionUsuario(data.value);
+          this.router.navigate(['pages']);
+        }else{
+          this._utilidadService.mostrarAlerta('No se encontraron coincidencias',"Opps!");
+        }
+      },
+      complete:()=>{
+        this.mostrarLoading=false;
+      },
+      error:()=>{
+        this._utilidadService.mostrarAlerta('Hubo un error',"Opps!");
+      }
+    })
+  }
 }
